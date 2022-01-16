f3c1abb12f2811a68f2c15a5ab1a349c275b9b0f
Thiago Henrique <thaegohen001@gmail.com>
2022-01-16 19:33:30 -0300

    versionamento ts

--- TypeScript/versionamento.ts
+++ TypeScript/versionamento.ts
@@ -3,8 +3,27 @@ let total = 0;
 // tslint:disable-next-line:prefer-for-of
 for (let index = 0; index < valores.length; index++) {
     const element = valores[index];
-    if (index % 3 === 0) {
+    // console.log(index % 2);
+    if (index % 2 === 1) {
         total += element;
     }
+    // console.log("total " + total);
 }
 console.log("Total: " + total);
+
+//0
+//10 - 10
+//16 - 26
+//8  - 34
+
+//1
+//11
+//54
+
+
+//2
+//16
+//22
+
+//10, 11, 16, 20, 54, 22, 8, 2
+//  , 11,   , 31,   , 52,  , 55 
